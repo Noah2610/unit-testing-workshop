48991e8fd126b5feaad585925b0b4e0d803c18e6
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2022-12-15 14:48:00 +0100

    Rewrite tests to use describe and it

--- app.test.js
+++ app.test.js
@@ -2,24 +2,26 @@ const fs = require("fs");
 const path = require("path");
 const { saveDocument } = require("./app.js");
 
-test("document is saved to file", () => {
-    const name = "myDocumentName";
-    const body = "Hello Document From Test";
-    saveDocument(name, body);
+describe("saveDocument function", () => {
+    it("saves the document to file", () => {
+        const name = "myDocumentName";
+        const body = "Hello Document From Test";
+        saveDocument(name, body);
 
-    const filePath = path.resolve(`./documents/${name}.txt`);
-    const doesFileExist = fs.existsSync(filePath);
+        const filePath = path.resolve(`./documents/${name}.txt`);
+        const doesFileExist = fs.existsSync(filePath);
 
-    expect(doesFileExist).toBe(true);
+        expect(doesFileExist).toBe(true);
+    });
+
+    it("saves the document with the expected body", () => {
+        const name = "myDocumentName";
+        const body = "Hello Document From Test";
+        saveDocument(name, body);
+
+        const filePath = path.resolve(`./documents/${name}.txt`);
+        const fileContent = fs.readFileSync(filePath, "utf-8");
+
+        expect(fileContent).toBe(body);
+    });
 });
-
-test("document has body with expected content", () => {
-    const name = "myDocumentName";
-    const body = "Hello Document From Test";
-    saveDocument(name, body);
-
-    const filePath = path.resolve(`./documents/${name}.txt`);
-    const fileContent = fs.readFileSync(filePath, "utf-8");
-
-    expect(fileContent).toBe(body);
-})
